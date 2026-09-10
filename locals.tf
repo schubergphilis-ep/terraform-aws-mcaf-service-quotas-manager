a2d3a46be042006eab102ee4c34b7a6681550389
carlovoSBP <134266377+carlovoSBP@users.noreply.github.com>
2026-09-10 15:09:02 +0200

    feat: default to latest powertools layer arn (#9)
    * feat: default to latest powertools layer arn
    
    * chore(readme): Update module usage
    
    * fix: more informative powertool layer description
    
    * chore(readme): Update module usage
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -3,8 +3,20 @@ locals {
   account_region = var.region != null ? var.region : data.aws_region.current.region
 
   has_increase_config = sum([for item in var.quotas_manager_configuration : (item.quota_increase_config == null ? 0 : length(item.quota_increase_config))]) > 0
+
+  powertools_layer_parts = split(":", data.aws_ssm_parameter.powertools_layer_arn.value)
+
+  powertools_layer_arn = (
+    var.powertools_layer_version != null
+    ? "${join(":", slice(local.powertools_layer_parts, 0, length(local.powertools_layer_parts) - 1))}:${var.powertools_layer_version}"
+    : data.aws_ssm_parameter.powertools_layer_arn.value
+  )
 }
 
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
+
+data "aws_ssm_parameter" "powertools_layer_arn" {
+  name = "/aws/service/powertools/python/x86_64/python3.11/latest"
+}
--- variables.tf
+++ variables.tf
@@ -126,3 +126,9 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "powertools_layer_version" {
+  description = "The version of the Powertools Lambda layer to use. If omitted the latest version is used."
+  type        = string
+  default     = null
+}
